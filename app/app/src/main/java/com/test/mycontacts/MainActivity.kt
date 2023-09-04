03a9466cd0f7bfbead72e6586566137ce6e4ecbd
dklee1619 <dklee1619@naver.com>
2023-09-04 21:06:25 +0900

    "상세페이지 레이아웃"

--- app/app/src/main/java/com/test/mycontacts/MainActivity.kt
+++ app/app/src/main/java/com/test/mycontacts/MainActivity.kt
@@ -2,10 +2,42 @@ package com.test.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.test.mycontacts.databinding.ActivityMainBinding
+import com.test.mycontacts.databinding.FragmentContactDetailBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.apply {
+            ContactdetailBtn.setOnClickListener{
+                setFragment(ContactDetailFragment())
+            }
+            MypageBtn.setOnClickListener {
+                setFragment(MyPageFragment())
+            }
+            messageBtn.setOnClickListener{
+
+            }
+            callBtn.setOnClickListener {
+
+            }
+        }
+        setFragment(ContactDetailFragment())
+    }
+    private fun setFragment(frag : Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.frameLayout, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
     }
 }
+
+
+// 1.
