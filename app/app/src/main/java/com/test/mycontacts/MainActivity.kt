30f3944e52dd92ac8a487f4cdab34f93fb829e71
suyeno <realsu_yeonini418@naver.com>
2023-09-04 17:17:13 +0900

    [UI] : AddDialog 다이얼로그 UI 틀 완성

--- app/app/src/main/java/com/test/mycontacts/MainActivity.kt
+++ app/app/src/main/java/com/test/mycontacts/MainActivity.kt
@@ -2,10 +2,30 @@ package com.test.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.test.mycontacts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener(this)
     }
+
+    override fun onClick(view: View){
+        when(view?.id)
+        {
+            binding.button.id ->{
+
+            }
+        }
+
+    }
+
 }
