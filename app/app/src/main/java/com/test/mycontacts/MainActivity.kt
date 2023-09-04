69500982741edf9932a130e654077ca09e2d9700
dklee1619 <dklee1619@naver.com>
2023-09-05 00:43:04 +0900

    [DESIGN] 레이아웃 약간 수정

--- app/app/src/main/java/com/test/mycontacts/MainActivity.kt
+++ app/app/src/main/java/com/test/mycontacts/MainActivity.kt
@@ -5,39 +5,10 @@ import android.os.Bundle
 import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
-import com.test.mycontacts.databinding.ActivityMainBinding
-import com.test.mycontacts.databinding.FragmentContactDetailBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        binding.apply {
-            ContactdetailBtn.setOnClickListener{
-                setFragment(ContactDetailFragment())
-            }
-            MypageBtn.setOnClickListener {
-                setFragment(MyPageFragment())
-            }
-            messageBtn.setOnClickListener{
-
-            }
-            callBtn.setOnClickListener {
-
-            }
-        }
-        setFragment(ContactDetailFragment())
-    }
-    private fun setFragment(frag : Fragment) {
-        supportFragmentManager.commit {
-            replace(R.id.frameLayout, frag)
-            setReorderingAllowed(true)
-            addToBackStack("")
-        }
+        setContentView(R.layout.activity_main)
     }
 }
-
-
-// 1.
