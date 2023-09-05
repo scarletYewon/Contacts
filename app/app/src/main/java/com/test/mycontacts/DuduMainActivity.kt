18f6eb47b5b0766cd199a5b63e43f08200a89eae
limduhyoung <limduh2@naver.com>
2023-09-05 12:57:42 +0900

    fragment_sm.xml,fragment_jyp.xml 레이아웃

---
+++ app/app/src/main/java/com/test/mycontacts/DuduMainActivity.kt
@@ -0,0 +1,52 @@
+package com.test.mycontacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.ex6_simplelistview.MyAdapter
+import com.test.mycontacts.databinding.ActivityMainBinding
+
+class DuduMainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 데이터 원본 준비
+        val dataList = mutableListOf(
+            MyItems.SmItem(R.drawable.sm_taeyeon, "우윳빛깔 김태연", R.drawable.img_like),
+            MyItems.SmItem(R.drawable.sm_sunny, "사장님조카 써니", R.drawable.img_like),
+            MyItems.SmItem(R.drawable.sm_tiffany, "보석닮은 티파니", R.drawable.img_like),
+            MyItems.jypItem(R.drawable.jyp_lily, "요정 릴리", R.drawable.img_like2),
+            MyItems.SmItem(R.drawable.sm_hyoyeon, "춤신춤왕 효연", R.drawable.img_like),
+            MyItems.jypItem(R.drawable.jyp_haewon, "시원시원 해원", R.drawable.img_like2),
+            MyItems.SmItem(R.drawable.sm_taeyeon, "재태크왕 유리", R.drawable.img_like),
+            MyItems.jypItem(R.drawable.jyp_sullyoon, "설빙조아 설윤", R.drawable.img_like2),
+            MyItems.SmItem(R.drawable.sm_sooyoung, "swimming 수영", R.drawable.img_like),
+            MyItems.jypItem(R.drawable.jyp_bae, "베이비 배이", R.drawable.img_like2),
+            MyItems.jypItem(R.drawable.jyp_lily, "요정 릴리", R.drawable.img_like2),
+            MyItems.SmItem(R.drawable.sm_yoona, "얼굴천재 윤아", R.drawable.img_like),
+            MyItems.jypItem(R.drawable.jyp_lily, "피카츄의왕 지우", R.drawable.img_like2),
+            MyItems.jypItem(R.drawable.jyp_kyujin, "귀엽다 규진", R.drawable.img_like2),
+            MyItems.SmItem(R.drawable.sm_seohyun, "이쁜막내 서현", R.drawable.img_like)
+
+            )
+        // 어댑터 생성 및 연결
+        val adapter = MyAdapter(dataList)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter.itemClick = object : MyAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val name: String = (dataList[position] as MyItems.SmItem).aName
+                Toast.makeText(this@DuduMainActivity," $name 선택!", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+}
--- app/app/src/main/java/com/test/mycontacts/ItemAdapter.kt
+++ app/app/src/main/java/com/test/mycontacts/ItemAdapter.kt
@@ -1,42 +1,72 @@
-package com.test.mycontacts
+package com.android.ex6_simplelistview
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.test.mycontacts.databinding.ItemRecyclerviewBinding
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
+import com.test.mycontacts.MyItems
+import com.test.mycontacts.databinding.FragmentJypBinding
+import com.test.mycontacts.databinding.FragmentSmBinding
 
-class ItemAdapter(val mItems: MutableList<MyItems>) : RecyclerView.Adapter<ItemAdapter.Holder>() {
+class MyAdapter(val mItems: MutableList<MyItems>) : RecyclerView.Adapter<ViewHolder>() {
+
+    companion object {
+        private const val VIEW_TYPE_SM = 1
+        private const val VIEW_TYPE_JYP = 2
+
+    }
 
     interface ItemClick {
-        fun onClick(view : View, position : Int)
+        fun onClick(view: View, position: Int)
     }
 
-    var itemClick : ItemClick? = null
+    var itemClick: ItemClick? = null
 
+    //3.ViewGroup으로 recycleview가 들어올거고, 그다음 int값으로 VIEW_TYPE_SM = 1이나 VIEW_TYPE_JYP = 2가 들어온다
+    //
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            VIEW_TYPE_SM -> {
+                val binding =
+                    FragmentSmBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                SmViewHolder(binding)
+            }
 
-    //2. 그 홀더를 onCreateViewHolder에서 리턴하게 된다.(item_Recyclerview LayOut을 가져온다.)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding = ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return Holder(binding)
-    }
-
-
-
-//3. activity_main.xml  리스트에서 한 개의 항목을 불러올때 마다 MyAdapter.kt에서 onBindviewHolder가 생성이 된다.
-//    그러면 그 holder랑 position을 받아서 그 해당 홀더에 아이콘 사진을 넣고 name과 age를 집어넣게 된다.
-//    전에 listView에서는 getView를 했었는데 그거랑 같은 기능이라고 생각하면 쉽다.
-
-
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.itemView.setOnClickListener {  //클릭이벤트추가부분
-            itemClick?.onClick(it, position)
+            else -> {
+                //뷰타입 하나 일땐 이거만 있었다.
+                val binding =
+                    FragmentJypBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                JypViewHolder(binding)
+            }
         }
-        holder.iconImageView.setImageResource(mItems[position].aIcon)
-        holder.name.text = mItems[position].aName
-        holder.like.setImageResource(mItems[position.alike])
+    }
+
+    //실제로 화면이 실행됐을때 한 줄씩 불러준다.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (val item = mItems[position]) {
+            is MyItems.SmItem -> {
+                (holder as SmViewHolder).iconImageView.setImageResource(item.aIcon)
+                holder.name.text = "${item.aName} "
+                holder.iconImageView.setImageResource(item.alike1)
+            }
+            is MyItems.jypItem -> {
+                (holder as JypViewHolder).iconImageView.setImageResource(item.bIcon)
+                holder.name.text = "${item.bName}"
+                holder.iconImageView.setImageResource(item.blike2)
+
+                holder.itemView.setOnClickListener {
+                    itemClick?.onClick(it, position)
+                }
+            }
+        }
     }
 
     override fun getItemId(position: Int): Long {
@@ -47,14 +77,26 @@ class ItemAdapter(val mItems: MutableList<MyItems>) : RecyclerView.Adapter<ItemA
         return mItems.size
     }
 
+    //2.position에 해당하는 타입이 뭔지 알려달라는 함수
+    override fun getItemViewType(position: Int): Int {
+        return when (mItems[position]) {
+            is MyItems.SmItem -> VIEW_TYPE_SM
+            is MyItems.jypItem -> VIEW_TYPE_JYP
+        }
+    }
 
-
-
-
-    //1.일단 홀더를 만들고
-    inner class Holder(val binding: ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root) {
+    //1. inner class에 holder를 만드는데 2가지 레이아웃으로 뿌려줘야 하니까 2가지 Holder를 만든다.
+    inner class JypViewHolder(binding: FragmentJypBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         val iconImageView = binding.iconItem
         val name = binding.textItem
-        val like = binding.lllike
+        val like = binding.like
+    }
+
+    inner class SmViewHolder(binding: FragmentSmBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val iconImageView = binding.iconItem
+        val name = binding.textItem
+        val like = binding.like
     }
 }
