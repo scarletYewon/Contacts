b42cae52248043f33b722295fcb06a9085c691ad
suyeno <realsu_yeonini418@naver.com>
2023-09-05 14:07:57 +0900

    [FEAT] : 다이얼로그 기능 구현 (버튼 클릭시 다이얼로그 생성/다이얼로그 값 메인으로 넘기기)

--- app/app/src/main/java/com/test/mycontacts/AddDialog.kt
+++ app/app/src/main/java/com/test/mycontacts/AddDialog.kt
@@ -1,14 +1,45 @@
 package com.test.mycontacts
 
+
 import android.app.Dialog
-import android.view.Window
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
 import com.test.mycontacts.databinding.DialogBinding
 
-class AddDialog(private val context : MainActivity) {
+class AddDialog(context: Context, private val binding: DialogBinding) : Dialog(context) {
 
-    private lateinit var binding : DialogBindin
+    private lateinit var onClickedListener: ButtonClickListener
 
+    fun dig() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(binding.root)
+        setCanceledOnTouchOutside(false) // 다이얼로그 외의 부분을 눌렀을 때 꺼지지 않게 설정
+        window!!.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
 
-    fun show(content : String) {}
+        val saveButton = binding.save
+        val cancelButton = binding.cancelbtn
+
+        saveButton.setOnClickListener {
+            val name = binding.name.text.toString()
+            val number = binding.number.text.toString()
+            val mail = binding.mail.text.toString()
+
+            onClickedListener.onClicked(name, number, mail)
+            dismiss()
+        }
+
+        cancelButton.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    interface ButtonClickListener {
+        fun onClicked(name: String, number: String, email: String)
+    }
+
+    fun setOnButtonClickListener(listener: ButtonClickListener) {
+        onClickedListener = listener
+    }
+}
+
--- app/app/src/main/java/com/test/mycontacts/MainActivity.kt
+++ app/app/src/main/java/com/test/mycontacts/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.test.mycontacts
 
-import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.test.mycontacts.databinding.ActivityMainBinding
+import com.test.mycontacts.databinding.DialogBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var dialogBinding: DialogBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener(this)
-    } // requirecontext()
+        val result = binding.textt
+        val button = binding.button
 
+        button.setOnClickListener {
+            dialogBinding = DialogBinding.inflate(layoutInflater)
+            val dialog = AddDialog(this, dialogBinding)
+
+            dialog.dig()
+
+            dialog.setOnButtonClickListener(object : AddDialog.ButtonClickListener {
+                override fun onClicked(name: String, number: String, mail: String) {
+                    val text = "$name $number $mail"
+                    result.text = text
+                }
+            })
+
+            dialog.show()
+        }
+
+    }
 }
-
