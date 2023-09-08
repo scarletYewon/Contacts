106c46df4535aeca09a8d4310c5d21d1a34c6f51
dklee1619 <dklee1619@naver.com>
2023-09-08 20:36:05 +0900

    [LITTLE_FIX] : 🔧잔잔바리 수정

--- app/app/src/main/java/com/test/mycontacts/MyItems.kt
+++ app/app/src/main/java/com/test/mycontacts/MyItems.kt
@@ -15,21 +15,21 @@ sealed class MyItems {
     }
       companion object { // 데이터 초기화를 피하기 위해 ContactList에서 data class로 옮기고 싱글턴으로 만듬
         var defaultDataList = mutableListOf<MyItems>(
-            Item(R.drawable.sm_taeyeon, "Tae Yeon", "010-0000-0000", "taeyeon@sm.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.sm_sunny, "Sunny", "010-0000-0001", "sunny@sm.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.sm_tiffany, "Tiffany", "010-0000-0002", "tiffany@sm.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.jyp_lily, "Lily ", "010-0000-0003", "lily@jyp.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.sm_hyoyeon, "Hyo Yeon", "010-0000-0004", "hyoyeon@sm.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.jyp_haewon, "Hae Won", "010-0000-0005", "haewon@jyp.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.sm_yuri, "Yuri", "010-0000-0006", "yuri@sm.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.jyp_sullyoon, "Sull Yoon", "010-0000-0007", "sullyoon@jyp.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.sm_sooyoung, "Soo Young", "010-0000-0008", "sooyoung@sm.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.jyp_bae, "Bae", "010-0000-0009", "bae@jyp.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.jyp_lily, "Lily", "010-0000-0010", "lily@jyp.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.sm_yoona, "Yoon Ah", "010-0000-0011", "yoona@sm.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.jyp_jiwoo, "Ji Woo", "010-0000-0012", "jiwoo@jyp.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.jyp_kyujin, "Kyu Jin", "010-0000-0013", "kyujin@jyp.kr", R.drawable.img_like3,0,0),
-            Item(R.drawable.sm_seohyun, "Seo Hyun", "010-0000-0014", "seohyun@sm.kr", R.drawable.img_like3,0,0)
+            Item(drawableuri(R.drawable.sm_taeyeon), "Tae Yeon", "010-0000-0000", "taeyeon@sm.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.sm_sunny), "Sunny", "010-0000-0001", "sunny@sm.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.sm_tiffany), "Tiffany", "010-0000-0002", "tiffany@sm.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.jyp_lily), "Lily ", "010-0000-0003", "lily@jyp.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.sm_hyoyeon), "Hyo Yeon", "010-0000-0004", "hyoyeon@sm.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.jyp_haewon), "Hae Won", "010-0000-0005", "haewon@jyp.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.sm_yuri), "Yuri", "010-0000-0006", "yuri@sm.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.jyp_sullyoon), "Sull Yoon", "010-0000-0007", "sullyoon@jyp.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.sm_sooyoung), "Soo Young", "010-0000-0008", "sooyoung@sm.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.jyp_bae), "Bae", "010-0000-0009", "bae@jyp.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.jyp_lily), "Lily", "010-0000-0010", "lily@jyp.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.sm_yoona), "Yoon Ah", "010-0000-0011", "yoona@sm.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.jyp_jiwoo), "Ji Woo", "010-0000-0012", "jiwoo@jyp.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.jyp_kyujin), "Kyu Jin", "010-0000-0013", "kyujin@jyp.kr", R.drawable.img_like3,0,0),
+            Item(drawableuri(R.drawable.sm_seohyun), "Seo Hyun", "010-0000-0014", "seohyun@sm.kr", R.drawable.img_like3,0,0)
         )
           fun drawableuri(drawable:Int):Uri?{
               return Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + drawable)
